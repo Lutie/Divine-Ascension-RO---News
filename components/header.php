980d4f1ddf6870b72bc9d5406d59bc32cf1e5b27
Lutie <lutie66@gmail.com>
2020-09-08 01:04:57 +0200

    Add logo and change header's text

--- components/header.php
+++ components/header.php
@@ -1,20 +1,20 @@
 <header id="header">
     <div class="logo">
-        <span class="icon fa-gem"></span>
+        <img class="logo-img" src="images/logo.png"/>
     </div>
     <div class="content">
         <div class="inner">
             <h1>Divine Ascension Ragnarok Online</h1>
-            <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-            for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+            <p>Content will be upgraded soon with news and insight about the new project !<br />
+            For the moment you already can follow us on <a href="https://www.facebook.com/dgroserv">Facebook</a>.</p>
         </div>
     </div>
     <nav>
         <ul>
-            <li><a href="#features">Main features</a></li>
+            <li><a href="#features">Main Infos</a></li>
             <li><a href="#events">Events</a></li>
-            <li><a href="#contents">Custom contents</a></li>
-            <li><a href="#skills">Custom skills</a></li>
+            <li><a href="#contents">Contents</a></li>
+            <li><a href="#skills">Skills</a></li>
             <li><a href="#gameplay">Gameplay</a></li>
             <li><a href="#aboutus">About us</a></li>
             <li><a href="#elements">Elements sample</a></li>
