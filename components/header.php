f10b1ab142bba0a1ce13d3afd97f5360270beb43
Alexandre Jose <a.jose@sysdream.com>
2020-09-15 13:12:38 +0200

    update

--- components/header.php
+++ components/header.php
@@ -7,15 +7,13 @@
             <h1>Divine Ascension Ragnarok Online</h1>
             <p>DARO is a custom pre-renewal server for Ragnarok Online.</p>
             <p>Our goal is to keep the original gameplay sensations but with new contents !<br />
-            <p>Since this is the 5th Mystic Project, we have a lot of experience on the matter.<br />
-            <p>Every thing you need to know about this project will be displayed here.<br />
-            For the moment you already can follow us on <a href="https://www.facebook.com/dgroserv">Facebook</a>.</p>
+            <p>Follow us on our <a href="https://www.facebook.com/dgroserv">Facebook</a>.</p>
         </div>
     </div>
     <nav>
         <ul>
             <li><a href="#news">News</a></li>
-            <li><a href="#features">Config</a></li>
+            <li><a href="#features">Configs</a></li>
             <li><a href="#contents">Contents</a></li>
             <li><a href="#events">Events</a></li>
             <li><a href="#gameplay">Gameplay</a></li>
